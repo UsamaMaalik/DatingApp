b03bec0094e124065a6aa9289b9e5c114dc17e72
UsamaMaalik <usama_malik96@hotmail.com>
2021-12-12 20:26:50 +0500

    BootStrap Added and Just SSL Folder ADD

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,7 @@ namespace API
             services.AddDbContext<DataContext>( options => options.UseSqlServer(_config.GetConnectionString("DefaultConnections")));
             services.AddControllers();
             services.AddScoped<API.IService.IAppUser,API.Mvvm.AppUser>();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -49,6 +50,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200") );
 
             app.UseAuthorization();
 
